0a4c00d9e34ebaa257f337070da487637c1e03ba
saileshkasaju <leosailesh@gmail.com>
2017-06-03 20:34:39 +0545

    configure store with saga

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,3 +1,20 @@
 /**
  * Created by Edge on 6/3/2017.
  */
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../reducers';
+import rootSaga from '../sagas';
+
+
+
+const configureStore = () => {
+    const sagaMiddleware = createSagaMiddleware();
+    return {
+        ...createStore(rootReducer,
+            applyMiddleware(sagaMiddleware)),
+        runSaga: sagaMiddleware.run(rootSaga)
+    };
+};
+
+export default configureStore;
