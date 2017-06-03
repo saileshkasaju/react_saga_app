57817281b0a740ba1fdf6535ba3f671336b66d2d
saileshkasaju <leosailesh@gmail.com>
2017-06-03 20:51:45 +0545

    get images and videos from store

--- src/containers/MediaGalleryPage.js
+++ src/containers/MediaGalleryPage.js
@@ -3,20 +3,36 @@
  */
 import React from 'react';
 import { flickrImages, shutterStockVideos } from '../Api/api';
+import { connect } from 'react-redux';
+import { searchMediaAction } from '../actions/mediaActions';
+import PropTypes from 'prop-types';
 
 // MediaGalleryPage Component
 class MediaGalleryPage extends React.Component {
 
     // We want to get images and videos from the API right after our component renders.
     componentDidMount() {
-        flickrImages('rain').then(images => console.log(images, 'Images'));
-        shutterStockVideos('rain').then(videos => console.log(videos,'Videos'));
+        this.props.dispatch(searchMediaAction('rain'));
     }
 
     render() {
+        console.log(this.props.images, 'Images');
+        console.log(this.props.videos, 'Videos');
+        console.log(this.props);
         // TODO: Render videos and images here
         return (<div></div>)
     }
 }
 
-export default MediaGalleryPage;
+MediaGalleryPage.propTypes = {
+
+};
+
+const mapStateToProps = ({ images, videos }) => ({
+    images: images[0],
+    selectedImage: images.selectedImage,
+    videos: videos[0],
+    selectedVideo: images.selectedVideo
+});
+
+export default connect(mapStateToProps)(MediaGalleryPage);
