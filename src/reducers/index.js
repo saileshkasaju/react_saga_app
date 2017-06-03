abab277a83aae52ed0f34860479929108e85a6c4
saileshkasaju <leosailesh@gmail.com>
2017-06-03 20:32:23 +0545

    combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by Edge on 6/3/2017.
  */
+import { combineReducers } from 'redux';
+import images from './imageReducer';
+import videos from './videoReducer';
+
+
+const rootReducer = combineReducers({
+    images,
+    videos
+});
+
+export default rootReducer;
