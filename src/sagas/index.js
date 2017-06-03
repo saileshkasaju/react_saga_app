87462251be7723f3d46070a63f75e5b6a530b180
saileshkasaju <leosailesh@gmail.com>
2017-06-03 20:41:33 +0545

    now we export startForman as our rootSaga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,3 +1,11 @@
 /**
  * Created by Edge on 6/3/2017.
  */
+import { fork } from 'redux-saga/effects';
+import watchSearchMedia from './watcher';
+
+
+
+export default function* startForman() {
+    yield fork(watchSearchMedia);
+}
