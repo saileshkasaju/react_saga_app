cd0f075128dbb62e364adf64cc407d02af882d02
saileshkasaju <leosailesh@gmail.com>
2017-06-03 20:26:15 +0545

    add required action type constants

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,3 +1,11 @@
 /**
  * Created by Edge on 6/3/2017.
  */
+// It's preferable to keep your action types together.
+export const SELECTED_IMAGE = 'SELECTED_IMAGE';
+export const FLICKR_IMAGES_SUCCESS = 'FLICKR_IMAGES_SUCCESS';
+export const SELECTED_VIDEO = 'SELECTED_VIDEO';
+export const SHUTTER_VIDEOS_SUCCESS = 'SHUTTER_VIDEOS_SUCCESS';
+export const SEARCH_MEDIA_REQUEST = 'SEARCH_MEDIA_REQUEST';
+export const SEARCH_MEDIA_SUCCESS = 'SEARCH_MEDIA_SUCCESS';
+export const SEARCH_MEDIA_ERROR = 'SEARCH_MEDIA_ERROR';
