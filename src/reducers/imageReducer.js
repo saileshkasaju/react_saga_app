271d58b74d0b7a917687940c4464fa8a5ac60934
saileshkasaju <leosailesh@gmail.com>
2017-06-03 20:31:22 +0545

    add reducers

--- src/reducers/imageReducer.js
+++ src/reducers/imageReducer.js
@@ -1,3 +1,17 @@
 /**
  * Created by Edge on 6/3/2017.
  */
+import initialState from './initialState';
+import * as types from '../constants/actionTypes';
+
+
+export default function (state = initialState.images, action) {
+    switch (action.type) {
+        case types.FLICKR_IMAGES_SUCCESS:
+            return [...state, action.images];
+        case types.SELECTED_IMAGE:
+            return { ...state, selectedImage: action.image };
+        default:
+            return state;
+    }
+}
--- src/reducers/videoReducer.js
+++ src/reducers/videoReducer.js
@@ -1,3 +1,18 @@
 /**
  * Created by Edge on 6/3/2017.
  */
+import initialState from './initialState';
+import * as types from '../constants/actionTypes';
+
+
+
+export default function (state = initialState.videos, action) {
+    switch (action.type) {
+        case types.SHUTTER_VIDEOS_SUCCESS:
+            return [...state, action.videos];
+        case types.SELECTED_VIDEO:
+            return { ...state, selectedVideo: action.video };
+        default:
+            return state;
+    }
+}
