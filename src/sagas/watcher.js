e9e9e4d88714d177b36dbde3a8905a50ba344ca4
saileshkasaju <leosailesh@gmail.com>
2017-06-03 20:36:48 +0545

    add watcher for latest search

--- src/sagas/watcher.js
+++ src/sagas/watcher.js
@@ -1,3 +1,11 @@
 /**
  * Created by Edge on 6/3/2017.
  */
+import { takeLatest } from 'redux-saga/effects';
+import { searchMediaSaga } from './mediaSaga';
+import * as types from '../constants/actionTypes';
+
+
+export default function* watchSearchMedia() {
+    yield takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga);
+}
