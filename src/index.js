7ea8724227146a1b002bee9cc02d2eebaffd11af
saileshkasaju <leosailesh@gmail.com>
2017-06-03 20:44:09 +0545

    connect app to store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import routes from './routes';
 
+const store = configureStore();
 
 ReactDOM.render(
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('root')
 );
